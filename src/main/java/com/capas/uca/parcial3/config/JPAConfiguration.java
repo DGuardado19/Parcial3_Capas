3b74c1401896fa1cb9bf659a84dbd50ea1773419
David <00184217@uca.edu.sv>
2020-06-20 21:58:50 -0600

    Cambio ligero JPA

--- src/main/java/com/capas/uca/parcial3/config/JPAConfiguration.java
+++ src/main/java/com/capas/uca/parcial3/config/JPAConfiguration.java
@@ -2,27 +2,37 @@ package com.capas.uca.parcial3.config;
 
 import java.util.Properties;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableJpaRepositories(basePackages = "com.uca.capas.repositories")
+@EnableJpaRepositories(basePackages = "com.uca.capas.parcial3.repositories")
+@EnableTransactionManagement
 public class JPAConfiguration {
 	
+	@Bean
+	JpaTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory);
+		return transactionManager;
+	}
 	
 	@Bean
 	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
 		LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
 		em.setDataSource(dataSource());
 		em.setPersistenceUnitName("capas2");
-		em.setPackagesToScan("com.uca.capas.domain");
+		em.setPackagesToScan("com.uca.capas.parcial3.domain");
 		
 		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
 		em.setJpaVendorAdapter(vendorAdapter);
