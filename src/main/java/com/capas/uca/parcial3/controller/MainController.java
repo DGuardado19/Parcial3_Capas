3501834bc64b4139a5f0d2b505a84fae66e539c2
Roberto Hernandez <hernandez.roberto014@gmail.com>
2020-06-23 01:25:34 -0600

    Agregando un try-catch

--- src/main/java/com/capas/uca/parcial3/controller/MainController.java
+++ src/main/java/com/capas/uca/parcial3/controller/MainController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-
 import com.capas.uca.parcial3.domain.*;
 import com.capas.uca.parcial3.service.CentroEscolarService;
 import com.capas.uca.parcial3.service.DepartamentoService;
@@ -24,7 +23,7 @@ import com.capas.uca.parcial3.service.UsuarioService;
 
 @Controller
 public class MainController {
-	
+
 	@Autowired
 	private MateriaService MateriaService;
 	@Autowired
@@ -40,167 +39,167 @@ public class MainController {
 	@Autowired
 	private UsuarioService usuarioService;
 
-	
-	
 	@RequestMapping("/index")
 	public ModelAndView index() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("index");
 		return mav;
 	}
-	
+
 	@RequestMapping("/registro")
 	public ModelAndView registro() {
 		ModelAndView mav = new ModelAndView();
 		List<Departamento> departamentoLista = null;
 		List<Municipio> municipioLista = null;
-		//List<Usuario> usuarioLista = null;
+		// List<Usuario> usuarioLista = null;
 		try {
 			departamentoLista = departamentoService.findAll();
 			municipioLista = MunicipioService.findAll();
-			//usuarioLista = usuarioService.findAll();
-			
-		}catch(Exception e) {
+			// usuarioLista = usuarioService.findAll();
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		mav.addObject("departamentoLista", departamentoLista);
 		mav.addObject("municipioLista", municipioLista);
-		//mav.addObject("usuarioLista",usuarioLista);
+		// mav.addObject("usuarioLista",usuarioLista);
 		mav.setViewName("registroUsuario");
 		return mav;
 	}
-	
+
 	@RequestMapping("/tablaMaterias")
 	public ModelAndView tablaMaterias() {
 		ModelAndView mav = new ModelAndView();
 		List<Materia> materia = null;
 		try {
 			materia = MateriaService.findAll();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		mav.addObject("materiaList", materia);
 		mav.setViewName("tablaMateria");
 		return mav;
 	}
-	
+
 	@RequestMapping("/registroMateria")
 	public ModelAndView registroMateria() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("registroMateria");
 		return mav;
 	}
-	
+
 	@RequestMapping("/registroCentroEscolar")
 	public ModelAndView registroCentroEscolar() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("registroCentroEscolar");
 		return mav;
 	}
-	
+
 	@RequestMapping("/tablaCentroEscolar")
 	public ModelAndView tablaCentroEscolar() {
 		ModelAndView mav = new ModelAndView();
 		List<CentroEscolar> centroescolar = null;
 		try {
 			centroescolar = CentroEscolarService.findAll();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		mav.addObject("centro", centroescolar);
 		mav.setViewName("tablaCentroEscolar");
 		return mav;
 	}
-	
+
 	@RequestMapping("/tablaUsuario")
 	public ModelAndView tablaUsuario() {
-		
+
 		ModelAndView mav = new ModelAndView();
-		
+
 		List<Usuario> listaUsuario = null;
-		
+
 		try {
 			listaUsuario = usuarioService.findAll();
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		mav.addObject("usuario",listaUsuario);
+		mav.addObject("usuario", listaUsuario);
 		mav.setViewName("tablaUsuario");
 		return mav;
 	}
-	
+
 	@RequestMapping("/busquedaAlumno")
 	public ModelAndView busquedaAlumno() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("busquedaAlumno");
 		return mav;
 	}
-	
+
 	@RequestMapping("/tablaExpediente")
 	public ModelAndView tablaExpediente() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("tablaExpediente");
 		return mav;
 	}
-	
+
 	@RequestMapping("/registroAlumno")
 	public ModelAndView registroAlumno(@ModelAttribute Estudiante estudiante) {
 		ModelAndView mav = new ModelAndView();
 		try {
 			mostrarComboBox(mav);
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		mav.addObject("estudiante", estudiante);
 		return mav;
 	}
-	
+
 	@RequestMapping("/registroEstudiante")
-	public ModelAndView alumno(@Valid @ModelAttribute Estudiante estudiante,BindingResult result) {
+	public ModelAndView alumno(@Valid @ModelAttribute Estudiante estudiante, BindingResult result) {
 		ModelAndView mav = new ModelAndView();
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 			mostrarComboBox(mav);
-		}else {
+		} else {
 			try {
 				mostrarComboBox(mav);
 				estudianteService.insertAndUpdate(estudiante);
 				mav.addObject("estudiante", estudiante);
-			}catch(Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 		return mav;
 	}
-	
-	
+
 	public void mostrarComboBox(ModelAndView mav) {
-		
+
 		List<Departamento> departamentoLista = null;
 		List<Municipio> municipioLista = null;
 		List<CentroEscolar> centroescolar = null;
-		
-		centroescolar = CentroEscolarService.findAll();
-		departamentoLista = departamentoService.findAll();
-		municipioLista = MunicipioService.findAll();
-		
-		mav.addObject("departamentoLista", departamentoLista);
-		mav.addObject("municipioLista", municipioLista);
-		mav.addObject("centro", centroescolar);
-		
-		mav.setViewName("registroAlumno");
-		
+
+		try {
+			centroescolar = CentroEscolarService.findAll();
+			departamentoLista = departamentoService.findAll();
+			municipioLista = MunicipioService.findAll();
+
+			mav.addObject("departamentoLista", departamentoLista);
+			mav.addObject("municipioLista", municipioLista);
+			mav.addObject("centro", centroescolar);
+
+			mav.setViewName("registroAlumno");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
-	
+
 	@RequestMapping("/tablaMateriasCursadas")
 	public ModelAndView tablaMateriasCursadas() {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("tablaMateriasCursadas");
 		return mav;
 	}
-	
+
 	@RequestMapping("/registrarMateriaAlumno")
 	public ModelAndView registrarMateriaAlumno() {
 		ModelAndView mav = new ModelAndView();
